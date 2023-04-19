ed09ac2ace3efca02e5ce5f9d275a96fcfcc7909
Nidhi-PK <131234772+Nidhi-PK@users.noreply.github.com>
2023-04-19 12:23:11 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,46 +1,341 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include "stm32f7xx.h"
-#include "uart.h"
-#include "led.h"
 #include "adc.h"
 #include "tim.h"
-#include "stm32f767xx.h"
-#include "stm32f7xx.h"
+/* USER CODE END Includes */
 
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
 
-float sensor_value[10000];
-float y[10000]={0};
+/* USER CODE END PTD */
 
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
 
-int main(void)
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+int sensor_value[8][1000];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+#define selectpinsport GPIOA
+const int selectPins[3] = {S0_Pin, S1_Pin, S2_Pin};
+
+void selectMuxPin (int pin)
 {
-	SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2)); // set bits 20-23 for CP10 and CP11
-	uart3_tx_init();
-	pa4_adc1_init();
-	tim1_1hz_init();
-
-	while(1)
+	for (int z=0; z<3; z++)
 	{
-	for(int i =0; i<1000; i++)
-		{
-		  sensor_value[i] =  adc_get_data();
-		  while(!(TIM1->SR & SR_UIF)){}
-		  TIM1->SR &=~SR_UIF;
-
-		  for (int j = 2; j<1000; j++)
-		  {
-			  if (j>2)
-				  {
-				   //y[i] = 4096.00;
-			  		 y[j] = (double)(0.9847*sensor_value[i] - 1.8731*sensor_value[i-1] + 0.9847*sensor_value[i-2] + 1.8731*y[i-1] - 0.9695*y[i-2]);
-			  	   }
-		  }
-		  	  for (int k =0; k<1000; k++)
-		  	  {
-		  		  //printf("%dA\n",y[j]);
-		  		printf("%fA\n",y[j]);
-		  	  }
-
-		}
+		if (pin & (1<<z))
+			HAL_GPIO_WritePin(selectpinsport, selectPins[z], GPIO_PIN_SET);
+		else
+			HAL_GPIO_WritePin(selectpinsport, selectPins[z], GPIO_PIN_RESET);
 	}
 }
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  pa4_adc1_init();
+  tim1_1hz_init();
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  uint8_t rx_buffer[100];
+	  memset(rx_buffer, 0, sizeof(rx_buffer));
+
+	  HAL_UART_Receive(&huart2, rx_buffer, sizeof(rx_buffer), 100);        //receive data from UART
+
+	  for(int j=0;j<1000;j++)
+	  {
+		 for(int i=0;i<8;i++)
+		 {
+			selectMuxPin(i);
+
+			sensor_value[i][j] =  adc_get_data();
+		 }
+		 	selectMuxPin(0);
+			while(!(TIM1->SR & SR_UIF)){}
+			TIM1->SR &=~SR_UIF;
+	  }
+	  //HAL_UART_Transmit(&huart2, "START\r\n", sizeof("START\r\n"), 100);
+	  for (int j = 0; j < 10000; j++)
+	  	          {
+
+
+		  switch(rx_buffer[0])                                               // process the received data
+		  						{
+		  char buffer[10000];
+
+		  						  case '1':
+
+		  							  snprintf(buffer, sizeof(buffer), "%XEE%XAA%XAA%XAA%XAA%X%X%X%XFF\r\n", (int)sensor_value[0][j],(int)sensor_value[1][j],(int)sensor_value[2][j],(int)sensor_value[3][j],(int)sensor_value[4][j],(int)sensor_value[5][j],(int)sensor_value[6][j],(int)sensor_value[7][j]);
+		  							  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
+		  							  HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 100);
+
+		  							  //memset(sensor_value, 0, sizeof(sensor_value));
+		  							  break;
+		  						  default:
+		  							  break;
+
+		  						}
+
+
+	  	          }
+	  //HAL_UART_Transmit(&huart2, "STOP\r\n", sizeof("STOP\r\n"), 100);
+
+
+
+/*
+	  for(int j=0;j<1000;j++)
+	  {
+		  printf("%dA%dA%dA%dA%dA%dA%dA%dA\n",sensor_value[0][j],sensor_value[1][j],sensor_value[2][j],sensor_value[3][j],sensor_value[4][j],sensor_value[5][j],sensor_value[6][j],sensor_value[7][j]);
+	  }
+*/
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 96;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV6;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, S0_Pin|S1_Pin|S2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : S0_Pin S1_Pin S2_Pin */
+  GPIO_InitStruct.Pin = S0_Pin|S1_Pin|S2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PD8 PD9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+
+
+
+
+
