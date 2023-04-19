ba5e012f441ab21b1585c01c447bf7e275ef7144
Nidhi-PK <131234772+Nidhi-PK@users.noreply.github.com>
2023-04-19 12:33:43 +0530

    Update tim.c

--- tim.c
+++ tim.c
@@ -6,12 +6,12 @@
 
 void tim1_1hz_init(void)
 {
-	/*Enable clock access to TIM1*/
+							/*Enabling the  clock access to TIM1*/
 	 RCC->APB2ENR |=TIM1EN;
-	/*Set the prescaler*/
-	 TIM1->PSC =  16 - 1; // 16 000 000 / 1600 = 10 000
-	/*Set auto-relaod value*/
-	 TIM1->ARR =  1000 - 1;   //10 000 /10 000 = 1
-	/*Enable timer*/
-	 TIM1->CR1 |=CR1_CEN;
+	
+	 TIM1->PSC =  16 - 1; // 16 000 000 / 1600 = 10 000       // Set the prescaler
+	
+	 TIM1->ARR =  1000 - 1;   //10 000 /10 000 = 1             //Auto reload value
+	
+	 TIM1->CR1 |=CR1_CEN;                               //Enabling the timer.
 }
